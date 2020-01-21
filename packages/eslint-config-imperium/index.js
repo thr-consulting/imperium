f52a8a3f9a2a0aa8a2eb9b456dcd1f147a6de6fc
Matthew Sawatzky <34319870+msawatzky75@users.noreply.github.com>
2020-01-21 12:02:39 -0600

    allowed unused args instead of variables

--- packages/eslint-config-imperium/index.js
+++ packages/eslint-config-imperium/index.js
@@ -39,12 +39,12 @@ module.exports = {
 		'no-return-assign': ['error', 'except-parens'],
 		'no-console': ['error'],
 		'no-plusplus': 'off',
-		'no-unused-vars': ['error', {varsIgnorePattern: ['d', 'server', 'contextManager']}],
+		'no-unused-vars': ['error', {varsIgnorePattern: 'd', args: 'none'}],
 		'lines-between-class-members': ['error', 'always', {exceptAfterSingleLine: true}],
 
 		// Typescript
 		'@typescript-eslint/indent': ['off'],
-		'@typescript-eslint/no-unused-vars': ['error', {varsIgnorePattern: ['d', 'server', 'contextManager']}],
+		'@typescript-eslint/no-unused-vars': ['error', {varsIgnorePattern: 'd', args: 'none'}],
 		'@typescript-eslint/no-unused-expressions': ['off'], // This was disabled because some upgrade to airbnb rules.
 		'@typescript-eslint/explicit-member-accessibility': ['off'],
 		'@typescript-eslint/explicit-function-return-type': [
