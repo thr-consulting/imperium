3b27f6fa0f225970ce175cb9ea39ba0163ee3b90
Mike Kornelson <darkadept@durbn.net>
2020-03-09 11:57:59 -0500

    Implement React Refresh (#183)
    * Implemented react-refresh code via a babel plugin and webpack plugin. Fixes #147.
    
    * * Updated circleci
    * Only allow react-refresh babel plugin on development client builds.
    
    * updated test for eslint

--- packages/client/src/Root.tsx
+++ packages/client/src/Root.tsx
@@ -1,6 +1,5 @@
 import debug from 'debug';
 import React from 'react';
-import {hot} from 'react-hot-loader/root';
 import {IImperiumClient, RootProps} from './types';
 
 const d = debug('imperium.client.Root');
@@ -10,8 +9,6 @@ interface Props {
 	imperiumClient: IImperiumClient;
 }
 
-function Root(props: Props) {
+export default function Root(props: Props) {
 	return props.render(props);
 }
-
-export default hot(Root);
