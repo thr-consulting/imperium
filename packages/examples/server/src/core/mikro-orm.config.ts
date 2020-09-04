50b50b19c7ba3188e9fb511db824757ec5d2e162
darkadept <darkadept@durbn.net>
2020-09-04 11:56:13 -0500

    fix: switched mikroorm v4rc6

--- packages/examples/server/src/core/mikro-orm.config.ts
+++ packages/examples/server/src/core/mikro-orm.config.ts
@@ -1,23 +1,23 @@
+import debug from 'debug';
 import type {Options} from '@mikro-orm/core';
 import {PostgreSqlDriver} from '@mikro-orm/postgresql';
-// import {TsMorphMetadataProvider} from '@mikro-orm/reflection';
 // import path from 'path';
 // import {getInstalledPathSync} from 'get-installed-path';
 import {entities} from '@imperium/example-domain';
 
+const d = debug('imperium.example.server.mikro-orm.config');
+
 // const domainPath = getInstalledPathSync('@imperium/example-domain', {local: true, paths: process.mainModule.paths});
+// const a = path.relative(process.cwd(), path.join(domainPath, 'src'));
 
 export const mikroOrmConfig: Options = {
-	// metadataProvider: TsMorphMetadataProvider,
-	// entities: [path.resolve(domainPath, 'dist')],
-	// entitiesTs: [path.resolve(domainPath, 'src')],
 	entities: Object.values(entities),
 	clientUrl: process.env.POSTGRESQL_URL,
 	type: 'postgresql',
 	driver: PostgreSqlDriver,
 	debug: process.env.POSTGRESQL_LOGGING === 'true',
-	// discovery: {
-	// 	disableDynamicFileAccess: true,
-	// },
+	discovery: {
+		disableDynamicFileAccess: true,
+	},
 	// ...(process.env.POSTGRESQL_SSL_CA ? {ssl: {ca: process.env.POSTGRESQL_SSL_CA}} : {}),
 };
