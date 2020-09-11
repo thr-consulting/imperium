98ba3520c85167f80d87d6ca0881f8f1d6f69fce
darkadept <darkadept@durbn.net>
2020-09-11 16:01:51 -0500

    fix(example-standalone): updated the standalone example to pass all tests

--- packages/examples/standalone/src/index.ts
+++ packages/examples/standalone/src/index.ts
@@ -14,7 +14,9 @@ export async function main() {
 	d(val);
 
 	if (env.runAsService) {
-		const noop = Function(); // eslint-disable-line no-new-func
+		// eslint-disable-next-line @typescript-eslint/no-implied-eval, no-new-func
+		const noop = Function();
+		// eslint-disable-next-line @typescript-eslint/no-implied-eval
 		setInterval(noop, 10000);
 	} else {
 		await connectors.close();
