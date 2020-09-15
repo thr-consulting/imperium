404a93f102a27fabac97637671aeb83bf7a6494e
darkadept <darkadept@durbn.net>
2020-09-15 14:04:20 -0500

    fix(auth-graphql-client): fixed export

--- packages/auth-graphql-client/src/index.ts
+++ packages/auth-graphql-client/src/index.ts
@@ -1,7 +1,7 @@
 import type {ImperiumGraphqlClientModule} from '@imperium/graphql-client';
 import {AuthGraphqlClientOptions, createLinks} from './apolloLink';
 
-export default function authGraphqlClient(options?: AuthGraphqlClientOptions): ImperiumGraphqlClientModule {
+export function authGraphqlClientModule(options?: AuthGraphqlClientOptions): ImperiumGraphqlClientModule {
 	return {
 		name: '@imperium/auth-graphql-client',
 		apolloLinks: createLinks(options),
