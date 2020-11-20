23375b936d9c84904934e158e311528a47ab08a9
Mike Kornelson <darkadept@durbn.net>
2020-11-20 15:11:55 -0600

    feat(auth-server): added ability to pass the access token to an endpoint via a query parameter (#247)

--- packages/auth-server/src/middleware/authMiddleware.ts
+++ packages/auth-server/src/middleware/authMiddleware.ts
@@ -1,5 +1,6 @@
 import debug from 'debug';
 import jwt from 'express-jwt';
+import {verify, Algorithm} from 'jsonwebtoken';
 import {compose} from '@imperium/server';
 import type {RequestHandler} from 'express';
 import type {AuthMiddlewareConfig} from '../types';
@@ -12,13 +13,40 @@ export interface Auth {
 	id: string;
 }
 
+function urlParameterAuth(config: AuthMiddlewareConfig): RequestHandler {
+	return (req, res, next) => {
+		const {token} = req.query;
+
+		if (token && typeof token === 'string') {
+			// @ts-ignore
+			req.user = verify(token, env.authAccessTokenSecret, {
+				algorithms: env.authAccessTokenAlgorithms as Algorithm[],
+			});
+		} else if (config.credentialsRequired) {
+			throw new Error('Credentials are required');
+		}
+		next();
+	};
+}
+
 export function authMiddleware(config: AuthMiddlewareConfig): RequestHandler {
+	const middlewareArr: RequestHandler[] = [];
+
+	// Use either query token middleware or jwt header middleware
+	if (config.authQueryToken) {
+		middlewareArr.push(urlParameterAuth(config));
+	} else {
+		middlewareArr.push(
+			jwt({
+				secret: env.authAccessTokenSecret,
+				algorithms: env.authAccessTokenAlgorithms,
+				credentialsRequired: config.credentialsRequired === undefined ? false : config.credentialsRequired,
+			}),
+		);
+	}
+
 	return compose([
-		jwt({
-			secret: env.authAccessTokenSecret,
-			algorithms: env.authAccessTokenAlgorithms,
-			credentialsRequired: config.credentialsRequired === undefined ? false : config.credentialsRequired,
-		}),
+		...middlewareArr,
 		(req, res, next) => {
 			// @ts-ignore
 			if (req.user) {
