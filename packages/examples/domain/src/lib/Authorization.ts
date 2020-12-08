b98ed288a02fbc40da69fc927c8d4f6b4a562b9b
Mike Kornelson <darkadept@durbn.net>
2020-12-08 13:11:45 -0600

    Feature/upgrade (#248)
    * chore: upgraded packages
    
    * chore: updated packages

--- packages/examples/domain/src/lib/Authorization.ts
+++ packages/examples/domain/src/lib/Authorization.ts
@@ -1,33 +1,51 @@
-import {Ability, AbilityTuple, CanParameters, RawRuleOf} from '@casl/ability';
-import {packRules, permittedFieldsOf, unpackRules} from '@casl/ability/extra';
+import {Ability, AbilityTuple, CanParameters, RawRuleOf, RawRule} from '@casl/ability';
+import {PackRule, packRules, permittedFieldsOf, unpackRules} from '@casl/ability/extra';
 
-interface Authorization2Opts<A extends AbilityTuple, U> {
+interface AuthorizationOpts<A extends AbilityTuple, U> {
 	defineRulesFor: (user: U | null) => RawRuleOf<Ability<A>>[];
 	getUserById: (id: string) => Promise<U | null>;
-	setCache: (key: string, data: any, expire?: number) => Promise<typeof data>;
-	getCache: (key: string) => Promise<any>;
+	setCache: (key: string, data: unknown, expire?: number) => Promise<typeof data>;
+	getCache: (key: string) => Promise<unknown>;
+}
+
+/**
+ * Typeguard to check if a value is an array of PackRules.
+ * @param value
+ */
+function isPackedRules<T extends RawRule<never, never>>(value: unknown): value is PackRule<T>[] {
+	const packRuleArr = value as PackRule<T>[];
+	if (Array.isArray(packRuleArr)) {
+		if (packRuleArr.length === 0) return true;
+		const ret = packRuleArr.reduce((memo, packRule) => {
+			if (!memo) return false;
+			// I'm not fully checking to see if each array item of packRule complies with PackRule -mk
+			return !!Array.isArray(packRule);
+		}, false);
+		return ret;
+	}
+	return false;
 }
 
 export class Authorization<A extends AbilityTuple, U> {
 	public ability: Ability<A>;
-	private readonly id?: string;
+	private readonly _id?: string;
 
 	constructor(id?: string) {
-		this.id = id;
+		this._id = id;
 		this.ability = new Ability<A>();
 	}
 
-	public async prepare({defineRulesFor, getUserById, getCache, setCache}: Authorization2Opts<A, U>) {
-		const cacheKey = `imp:auth:rules:${this.id}`;
+	public async prepare({defineRulesFor, getUserById, getCache, setCache}: AuthorizationOpts<A, U>) {
+		const cacheKey = `imp:auth:rules:${this._id}`;
 		const packedRules = await getCache(cacheKey);
-		if (packedRules) {
+		if (packedRules && isPackedRules(packedRules)) {
 			const unpackedRules = unpackRules(packedRules);
 			this.ability = new Ability(unpackedRules);
 			return;
 		}
 
-		if (this.id) {
-			const u = await getUserById(this.id);
+		if (this._id) {
+			const u = await getUserById(this._id);
 			const rules = defineRulesFor(u);
 			await setCache(cacheKey, packRules(rules), 60);
 			this.ability = new Ability<A>(rules);
@@ -39,7 +57,7 @@ export class Authorization<A extends AbilityTuple, U> {
 			field.forEach(f => this.throwUnlessCan(action, subject, f));
 			return;
 		}
-		// @ts-ignore TODO I'm not sure what the correct Typescript is here.
+		// @ts-ignore I'm not sure what the correct Typescript is here as this has a lot to do with the complex typescript from the casl library. -mk
 		const result = this.ability.can(action, subject, field);
 		if (!result) throw new Error(`Unauthorized: ${action}`);
 	}
@@ -50,5 +68,9 @@ export class Authorization<A extends AbilityTuple, U> {
 
 	public permittedFieldsOf(action: A[0], subject: A[1]) {
 		return permittedFieldsOf(this.ability, action, subject);
+	}
+
+	public get id(): string | undefined {
+		return this._id;
 	}
 }
