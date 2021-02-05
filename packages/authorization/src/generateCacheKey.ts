7f78aa1a58d5fe035445a9ed2542f9fa5d3273b3
darkadept <darkadept@durbn.net>
2021-02-05 09:56:16 -0600

    fix: fixed cache name generation

--- packages/authorization/src/generateCacheKey.ts
+++ packages/authorization/src/generateCacheKey.ts
@@ -1,5 +1,5 @@
 import type {AbstractAuthSelector} from './AbstractAuthSelector';
 
 export function generateCacheKey(selector: AbstractAuthSelector, authId?: string) {
-	return `auth:level:${authId || 'unauthenticated'}:${selector.constructor.name}:${selector.getCacheId()}`;
+	return `auth:level:${authId || 'unauthenticated'}:${selector.getName()}:${selector.getCacheId()}`;
 }
