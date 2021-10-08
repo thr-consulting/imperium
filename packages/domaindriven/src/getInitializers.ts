5a6830f960468e58d7dce48d53f9a6e356e53bf6
darkadept <darkadept@durbn.net>
2021-10-08 10:22:43 -0500

    chore: docs and upgraded some packages

--- packages/domaindriven/src/getInitializers.ts
+++ packages/domaindriven/src/getInitializers.ts
@@ -9,6 +9,10 @@ type GetInitializersReturn<T extends GetInitializersOpts> = {
 	[key in keyof T]: Initializers<T[key]>;
 };
 
+/**
+ * Returns an object of repositories transformed into initializer functions only.
+ * @param repositories
+ */
 export function getInitializers<T extends GetInitializersOpts>(repositories: T): GetInitializersReturn<T> {
 	return repositories;
 }
