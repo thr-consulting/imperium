20e51523ef9f078b3e6652ae55696e1036e5f7ad
darkadept <darkadept@durbn.net>
2021-11-18 09:20:20 -0600

    feat(domaindriven): added options to AbstractController feat(domaindriven): made protected methods on AbstractRepository public

--- packages/domaindriven/src/AbstractController.ts
+++ packages/domaindriven/src/AbstractController.ts
@@ -2,18 +2,26 @@
 import type {Authorization} from '@imperium/authorization';
 import type {EntityManager} from '@mikro-orm/core';
 
+export interface AbstractControllerOptions {
+	preventFlush?: boolean;
+}
+
 export abstract class AbstractController<Repos, User> {
 	protected readonly repos: Repos;
 	private readonly em: EntityManager;
 	readonly authorization: Authorization<User>;
+	protected readonly opts?: AbstractControllerOptions;
 
-	protected constructor(repos: Repos, em: EntityManager, authorization: Authorization<User>) {
+	protected constructor(repos: Repos, em: EntityManager, authorization: Authorization<User>, opts?: AbstractControllerOptions) {
 		this.repos = repos;
 		this.em = em;
 		this.authorization = authorization;
+		this.opts = opts;
 	}
 
 	protected async flush() {
-		await this.em.flush();
+		if (!this.opts?.preventFlush) {
+			await this.em.flush();
+		}
 	}
 }
--- packages/domaindriven/src/index.ts
+++ packages/domaindriven/src/index.ts
@@ -2,3 +2,4 @@ export {AbstractController} from './AbstractController';
 export {AbstractRepository} from './AbstractRepository';
 export {getInitializers} from './getInitializers';
 export type {EntityBase} from './types';
+export type {AbstractControllerOptions} from './AbstractController';
