d8cb27637e0a230b9b0d7ec5e0a597f86cc6593a
darkadept <darkadept@durbn.net>
2021-12-20 15:25:04 -0600

    docs: added trailing slash

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'darkadept', // Usually your GitHub org/user name.
   projectName: 'imperium', // Usually your repo name.
+  trailingSlash: true,
 
   presets: [
     [
