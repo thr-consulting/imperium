664032e46599aff226c703cefbd5734fe4be3878
darkadept <darkadept@durbn.net>
2022-01-25 14:00:09 -0600

    chore: upgrades

--- packages/graphql-server/src/schema/GraphQLLocalTime.ts
+++ packages/graphql-server/src/schema/GraphQLLocalTime.ts
@@ -1,4 +1,4 @@
-import type {LocalTime} from '@js-joda/core';
+import {LocalTime} from '@js-joda/core';
 import {toLocalTime} from '@thx/date';
 import debug from 'debug';
 import {GraphQLError} from 'graphql/error';
@@ -7,7 +7,7 @@ import {GraphQLScalarType} from 'graphql/type';
 
 const d = debug('imperium.graphql-server.schema.GraphQLLocalTime');
 
-export const GraphQLLocalTime = new GraphQLScalarType({
+export const GraphQLLocalTime = new GraphQLScalarType<LocalTime, number>({
 	name: 'LocalTime',
 	description: 'JS-Joda LocalTime',
 	// Parses variable values (JSON => JSON) (value from client)
@@ -15,8 +15,10 @@ export const GraphQLLocalTime = new GraphQLScalarType({
 		return toLocalTime(value);
 	},
 	// Called when the value is being sent to the client (value sent to the client)
-	serialize(value: LocalTime) {
-		return value.toSecondOfDay();
+	serialize(value) {
+		if (value instanceof LocalTime) return value.toSecondOfDay();
+		if (typeof value === 'number') return value;
+		throw new GraphQLError(`Cannot serialize LocalTime value: ${'value'}.`);
 	},
 	// Parses GraphQL language AST into the value. (AST => JSON)
 	parseLiteral(ast): LocalTime {
