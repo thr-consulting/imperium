84219a4a082243617a5bdd4333122c65fbeb2a17
darkadept <darkadept@durbn.net>
2022-03-28 15:05:23 -0500

    fix(authorization): removed shim for dataloader in favor of patch

--- packages/authorization/src/index.ts
+++ packages/authorization/src/index.ts
@@ -1,5 +1,3 @@
-import 'setimmediate';
-
 export {Authorization} from './Authorization';
 export type {Permission, JsonArray, JsonValue, JsonObject, PermissionLookup, PermissionLookupOpts, AuthorizationCache, PermissionKey} from './types';
 export {noPermissionLookup} from './noPermissionLookup';
