d47142c75dacfbed5115772f7567e0e1f56e9a8f
Mike Kornelson <darkadept@durbn.net>
2022-08-12 12:13:53 -0500

    Feature/fix jwt error (#330)
    * fix(auth-client): hopefully fixes the jwt error finally
    
    * fix(auth-client): hopefully fixes the jwt error finally

--- packages/auth-client/src/hooks/useFetch.ts
+++ packages/auth-client/src/hooks/useFetch.ts
@@ -1,8 +1,12 @@
 import {env} from '@thx/env';
 import debug from 'debug';
+import decode from 'jwt-decode';
+import {useCallback, useContext} from 'react';
+import {AuthContext} from '../AuthContext';
 import {defaults} from '../defaults';
 import {injectNewAuthorization} from '../lib/injectNewAuthorization';
 import {fetchAccessToken, isTokenValidOrUndefined} from '../lib/storage';
+import type {AccessToken} from '../types';
 import {useLogout} from './useLogout';
 
 const d = debug('imperium.auth-client.hooks.useFetch');
@@ -11,25 +15,36 @@ const f = window.fetch;
 
 export function useFetch() {
 	const logout = useLogout();
+	const authCtx = useContext(AuthContext);
 
-	return async (input: RequestInfo, init?: RequestInit) => {
-		let newAccess: string | null = null;
-		if (!isTokenValidOrUndefined()) {
-			d('Token is invalid');
-			try {
-				const newToken = await fetchAccessToken();
-				const {access} = await newToken.json();
-				newAccess = access;
-				window.localStorage.setItem(env.getString('authAccessTokenKey', defaults.authAccessTokenKey), access);
-			} catch (err) {
-				d('There was a problem refreshing the access token. Re-login required.');
-				await logout();
+	return useCallback(
+		async (input: RequestInfo, init?: RequestInit) => {
+			if (!authCtx.authorization.id) return f(input, init);
+			let newAccess: string | null = null;
+			if (!isTokenValidOrUndefined()) {
+				d('Token is invalid');
+				try {
+					const newToken = await fetchAccessToken();
+					const {access} = await newToken.json();
+					newAccess = access;
+					if (!newAccess) throw new Error('New access token could not be retrieved');
+					window.localStorage.setItem(env.getString('authAccessTokenKey', defaults.authAccessTokenKey), access);
+					authCtx.setAuthenticated({id: authCtx.authorization.id, access: newAccess});
+					if (env.isDevelopment()) {
+						const y = decode(access) as AccessToken;
+						d(`Retrieved new access token. Expires: ${new Date(y.exp * 1000)}`);
+					}
+				} catch (err) {
+					d('There was a problem refreshing the access token. Re-login required.');
+					await logout();
+				}
 			}
-		}
 
-		if (newAccess) {
-			return f(input, injectNewAuthorization(newAccess, init));
-		}
-		return f(input, init);
-	};
+			if (newAccess) {
+				return f(input, injectNewAuthorization(newAccess, init));
+			}
+			return f(input, init);
+		},
+		[authCtx, logout],
+	);
 }
