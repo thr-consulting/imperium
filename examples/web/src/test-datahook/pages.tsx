3f35c2de2ed3b406c8fa4032fb527670835dcc83
Shayne Thiessen <shayne@shaynethiessen.com>
2025-03-21 09:07:22 -0500

    Option to hide sidebar (#363)
    * feat: option to hide sidebar
    
    * fix: lint

--- examples/web/src/test-datahook/pages.tsx
+++ examples/web/src/test-datahook/pages.tsx
@@ -3,14 +3,17 @@ import debug from 'debug';
 import {TestDatahookA} from './TestDatahookA';
 import {TestDatahookB} from './TestDatahookB';
 import {routes} from './routes';
+import {SampleSidebar} from '../SampleSidebar';
 
 const d = debug('imperium.web.test-datahook.pages');
 
 export const routeProps = createPages(routes, {
 	testDatahookA: {
 		content: () => <TestDatahookA />,
+		sidebar: [{render: SampleSidebar}],
 	},
 	testDatahookB: {
 		content: () => <TestDatahookB />,
+		sidebar: [{render: SampleSidebar}],
 	},
 });
