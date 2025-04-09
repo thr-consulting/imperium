a34238f34a2add7be11b64a910e7d5d82d238b9d
Spoowy <erwin.flaming@gmail.com>
2025-04-09 11:55:14 -0500

    feat(domaindriven): add cron schedule

--- packages/domaindriven/src/AbstractHeartbeat.ts
+++ packages/domaindriven/src/AbstractHeartbeat.ts
@@ -1,15 +1,18 @@
 interface AbstractHeartbeatCon<CTX> {
 	name: string;
+	cronSchedule?: string;
 	ctx: CTX;
 }
 
 export abstract class AbstractHeartbeat<T, CTX> {
-	protected constructor({name, ctx}: AbstractHeartbeatCon<CTX>) {
+	protected constructor({name, cronSchedule, ctx}: AbstractHeartbeatCon<CTX>) {
 		this.name = name;
+		this.cronSchedule = cronSchedule;
 		this.ctx = ctx;
 	}
 
 	public readonly name: string;
+	public readonly cronSchedule?: string; // cron schedule
 	public abstract select(): Promise<T[]>;
 	public abstract command(item: T): Promise<void>;
 
