466af85d83b81c02cd63a29bc775a426d6eb5007
Spoowy <erwin.flaming@gmail.com>
2025-10-22 11:37:18 -0500

    fix(graphql-server): fix inter-package LocalDate and LocalTime error

--- packages/graphql-server/src/schema/GraphQLLocalTime.ts
+++ packages/graphql-server/src/schema/GraphQLLocalTime.ts
@@ -1,4 +1,4 @@
-import {LocalTime} from '@js-joda/core';
+import {type LocalTime} from '@js-joda/core';
 import {toLocalTime} from '@thx/date';
 import debug from 'debug';
 import {GraphQLError, Kind, GraphQLScalarType} from 'graphql';
@@ -7,17 +7,24 @@ const d = debug('imperium.graphql-server.schema.GraphQLLocalTime');
 
 export const GraphQLLocalTime = new GraphQLScalarType<LocalTime, number>({
 	name: 'LocalTime',
-	description: 'JS-Joda LocalTime',
+	description: 'JS-Joda LocalTime serialized as seconds of day',
+
 	// Parses variable values (JSON => JSON) (value from client)
 	parseValue(value): LocalTime {
 		return toLocalTime(value);
 	},
+
 	// Called when the value is being sent to the client (value sent to the client)
-	serialize(value) {
-		if (value instanceof LocalTime) return value.toSecondOfDay();
-		if (typeof value === 'number') return value;
-		throw new GraphQLError(`Cannot serialize LocalTime value: ${'value'}.`);
+	serialize(value: unknown): number {
+		try {
+			if (typeof value === 'number') return value;
+			return toLocalTime(value).toSecondOfDay();
+		} catch (err: any) {
+			d('Error serializing LocalTime: %O', err);
+			throw new GraphQLError(`Cannot serialize LocalTime value: ${JSON.stringify(value)}`);
+		}
 	},
+
 	// Parses GraphQL language AST into the value. (AST => JSON)
 	parseLiteral(ast): LocalTime {
 		if (ast.kind === Kind.STRING) return toLocalTime(ast.value);
