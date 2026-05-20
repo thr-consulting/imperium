53dacb6d91be35d57b85bc6a5e141ee9606be1d7
Shayne Thiessen <shayne@shaynethiessen.com>
2026-05-20 11:54:15 -0500

    feat: revert changes

--- packages/state/src/index.ts
+++ packages/state/src/index.ts
@@ -13,4 +13,3 @@ export function stateClientModule(opts?: StateClientOptions): ImperiumClientModu
 export type {ImperiumStateClientModule, StateClientOptions} from './types';
 
 export {createSliceHook} from './createSliceHook';
-export {createStateSlice} from './createStateSlice';
