912fa626c51279785d05fecb2721309b6132b47e
Shayne Thiessen <shayne@shaynethiessen.com>
2026-05-20 09:51:27 -0500

    fix: add fallback, remove type casting

--- packages/state/src/withImperiumState.tsx
+++ packages/state/src/withImperiumState.tsx
@@ -1,5 +1,5 @@
 import type {Hoc, ImperiumClient} from '@imperium/client';
-import {configureStore, type Middleware, type ReducersMapObject, type Slice} from '@reduxjs/toolkit';
+import {configureStore, type ReducersMapObject, type Slice} from '@reduxjs/toolkit';
 import debug from 'debug';
 import type {ComponentType} from 'react';
 import {Provider} from 'react-redux';
@@ -52,7 +52,7 @@ export function withImperiumState(opts?: StateClientOptions) {
 				? configureStore({
 						reducer,
 						preloadedState,
-						middleware: getDefaultMiddleware => getDefaultMiddleware().concat(opts?.middleware as Middleware[]),
+						middleware: getDefaultMiddleware => getDefaultMiddleware().concat(opts?.middleware || []),
 					})
 				: null;
 
